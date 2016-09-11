3f19f76d4183ac617ed89e09f7e29b39276550c0
jasonpaulso <jason@jasonpaulsouthwell.com>
2016-09-10 18:57:03 -0700

    creating api for images, js get gallery function

--- app/controllers/gallery_controller.rb
+++ app/controllers/gallery_controller.rb
@@ -1,2 +1,11 @@
 class GalleryController < ApplicationController
+
+  def index
+    @gallery = Image.all
+    respond_to do |format|
+      format.html {render :index}
+      format.json {render json: @gallery}
+    end
+  end
+
 end
